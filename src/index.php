2dd16132b2cedc67f8bbb850fdcf8fd8bb454c7e
pp <ichikawa929@yahoo.co.jp>
2022-10-30 00:18:08 +0900

    feat: use line-bot-sdk-tiny
    https://github.com/line/line-bot-sdk-php/tree/master/line-bot-sdk-tiny

--- src/index.php
+++ src/index.php
@@ -1,20 +1,50 @@
 <?php
-require(__DIR__ . '/vendor/autoload.php');
-require(__DIR__ . '/app/include.php');
 
-use LINE\LINEBot;
-use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-$http_client = new CurlHTTPClient(Env::getValue('LINE_CHANNEL_ACCESS_TOKEN'));
-$bot = new LINEBot($http_client, ['channelSecret' => Env::getValue('LINE_CHANNEL_SECRET')]);
-$signature = $_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE];
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+require_once('./LINEBotTiny.php');
 
-foreach ($events as $event) {
-    $response = $bot->replyMessage(
-        $event->getReplyToken(),
-        new TextMessageBuilder($event->getText())
-    );
-}
+$channelAccessToken = '<your channel access token>';
+$channelSecret = '<your channel secret>';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
